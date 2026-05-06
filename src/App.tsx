d0a62e40d68583234241b946ee298eab34d49421
eurenderman-creator <eu.renderman@gmail.com>
2026-05-06 16:09:29 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
